05f91e4c673958d153186ddfa66c2330ed2fcb9c
towhid <niamaaktar@gmail.com>
2023-07-06 19:15:04 +0600

    all changes has been updated

---
+++ src/app/Data/OptionalTask.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ProductOne from "../../../public/amazonSpeaker.jpg";
+import ProductEight from "../../../public/applewatch.jpg";
+import ProductTwo from "../../../public/appleWatch6.jpg";
+import ProductThree from "../../../public/camera.jpg";
+import ProductFour from "../../../public/cocacola.jpg";
+import ProductFive from "../../../public/dslr.jpg";
+import ProductSix from "../../../public/pepsi.jpg";
+import ProductSeven from "../../../public/plant.jpg";
+import ProductNine from "../../../public/shose.jpg";
+import ProductTen from "../../../public/watch.jpg";
+
+export const OptionalTask = [
+    {
+        id : 1, 
+        image : MenOne,
+        name : "",
+    }
+]
