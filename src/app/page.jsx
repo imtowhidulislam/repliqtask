15e2996931e71ae47f9d51e607f7e46f1e9bf527
towhid <niamaaktar@gmail.com>
2023-07-06 07:58:10 +0600

    all hearder color turned into a gradient color

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -23,12 +23,15 @@ export default function Home() {
     <>
       <div>
         <div>
-          <div className="z-10 overflow-hidden bg-lime-200 py-12 mt-16 md:mt-0">
+          <div className="z-10 mt-16 overflow-hidden bg-lime-200 py-12 md:mt-0">
             <header className="container z-0 grid grid-cols-homepageLayoutHero1 place-items-center gap-12 md:gap-16 lg:gap-20">
               <div className="z-0 animate-moveUp">
-                <h2 className="text-left font-mono text-4xl font-bold capitalize text-lime-950 sm:text-5xl lg:text-7xl">
-                  shopping and department store
-                </h2>
+                <div>
+                  <h2 className="bg-gradient-to-r from-pink-500 to-purple-600 bg-clip-text text-left font-mono text-4xl font-extrabold uppercase text-transparent sm:text-5xl lg:text-7xl">
+                    shopping and department store
+                  </h2>
+                </div>
+
                 <p className="py-4 text-left text-sm text-lime-900 sm:text-base md:text-base">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Dolores eaque eligendi animi accusamus voluptatibus cupiditate
